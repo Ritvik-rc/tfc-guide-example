f9b6070a4820a0843727233007e96fb6679257a9
Ritvik-rc <77318928+Ritvik-rc@users.noreply.github.com>
2021-04-22 14:15:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 provider "aws" {
   region = var.aws_region
+  profile = "default"
 }
 resource "aws_instance" "app_server" {
-  ami           = "ami-0bcf5425cdc1d8a85"
+  ami = "ami-0bcf5425cdc1d8a85"
+  associate_public_ip = "true"
   instance_type = "t2.micro"
   key_name= "TestPair"
   subnet_id= "subnet-059515ea9d63c9af5"
